60bb7f1127a028653b130517ed35cd077658d1cc
Ramazan Khakimov <ramazankzn@gmail.com>
2022-12-02 09:03:48 +0300

    lessons 48,49

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,10 @@
 package org.example;
 
+import org.example.model.Person;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 /**
  * Hello world!
  *
@@ -8,6 +13,30 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
+
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
+        try {
+
+            session.beginTransaction();
+
+            Person person1 = new Person("Ram", 30);
+            Person person2 = new Person("Sergey", 30);
+            Person person3 = new Person("Rustem", 28);
+
+            session.save(person1);
+            session.save(person2);
+            session.save(person3);
+
+            Person person = session.get(Person.class, 1);
+            System.out.println("Имя: " + person.getName() + ", возвраст: " + person.getAge());
+
+            session.getTransaction().commit();
+        } finally {
+            sessionFactory.close();
+        }
+
+
     }
 }
--- src/main/java/org/example/model/Person.java
+++ src/main/java/org/example/model/Person.java
@@ -1,2 +1,52 @@
-package org.example.model;public class Person {
+package org.example.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Person")
+public class Person {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column (name = "name")
+    private String name;
+
+    @Column (name = "age")
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Person() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
